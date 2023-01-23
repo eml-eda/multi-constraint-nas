ea26a3d68721b989fbcb6ea9d39c3fe720d6ca45
ABurrello <burry94>
2023-01-23 16:47:58 +0100

    added scripts to run on servers without scheduler

--- run_flexnas.sh
+++ run_flexnas.sh
@@ -3,37 +3,22 @@
 size_target=$3
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-benchmark=$1
-if [[ "$1" == "icl" ]]; then
-export CUDA_VISIBILE_DEVICES=0
-epochs=500
 cd_size=$2
 cd_ops=$7
 latency_target=$4
 l=$8
 loss=$6
 elements=$5
+benchmark=$1
+if [[ "$1" == "icl" ]]; then
+epochs=500
 python icl_training.py --cd-size ${cd_size} --size-target ${size_target} --latency-target ${latency_target} --loss_elements ${elements} --loss_type ${loss} --cd-ops ${cd_ops} --epochs ${epochs} --l ${l} | tee log/srch_icl_${size_target}_${cd_ops}_${latency_target}_${cd_ops}_${l}_${elements}_${loss}.log
 fi
 if [[ "$1" == "vww" ]]; then
-export CUDA_VISIBILE_DEVICES=1
 epochs=50
-cd_size=$2
-cd_ops=$7
-latency_target=$4
-l=$8
-loss=$6
-elements=$5
 python vww_training.py --cd-size ${cd_size} --size-target ${size_target} --latency-target ${latency_target} --loss_elements ${elements} --loss_type ${loss} --cd-ops ${cd_ops} --epochs ${epochs} --l ${l} | tee log/srch_vww_${size_target}_${cd_ops}_${latency_target}_${cd_ops}_${l}_${elements}_${loss}.log
 fi
-if [[ "$1" == "kws" ]]; then
-export CUDA_VISIBILE_DEVICES=2
+if [[ "$1" == "kws" ]]; thenù
 epochs=36
-cd_size=$2
-cd_ops=$7
-latency_target=$4
-l=$8
-loss=$6
-elements=$5
 python kws_training.py --cd-size ${cd_size} --size-target ${size_target} --latency-target ${latency_target} --loss_elements ${elements} --loss_type ${loss} --cd-ops ${cd_ops} --epochs ${epochs} --l ${l} | tee log/srch_kws_${size_target}_${cd_ops}_${latency_target}_${cd_ops}_${l}_${elements}_${loss}.log
 fi
---
+++ server_multi_run.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+benchmark=$1
+gpu=$2
+l=( "const" "increasing" )
+loss=( "abs" "max" )
+elements=( "mem" "mem+lat" )
+if [[ "$1" == "icl" ]]; then
+    size_target=( 2.0e+04 4.0e+04 6.0e+04 )
+    cd_size=( 1.3e-05 5.9e-06 3.8e-06 )
+    latency_target=(2.0e+06 6.0e+06 1.0e+07 )
+    cd_ops=( 1.3e-5 5.9e-6 3.8e-6 )
+    if [[ gpu == 0 ]]; then
+        echo "Run on GPU 0"
+        export CUDA_VISIBLE_DEVICES=0
+        l="const"
+        loss="abs" 
+    fi
+    if [[ gpu == 1 ]]; then
+        echo "Run on GPU 1"
+        export CUDA_VISIBLE_DEVICES=1
+        l="increasing"
+        loss="abs" 
+    fi
+    if [[ gpu == 2 ]]; then
+        echo "Run on GPU 2"
+        export CUDA_VISIBLE_DEVICES=2
+        l="const"
+        loss="max" 
+    fi
+    if [[ gpu == 3 ]]; then
+        echo "Run on GPU 3"
+        export CUDA_VISIBLE_DEVICES=3
+        l="increasing"
+        loss="max" 
+    fi
+            for k in "${!elements[@]}"
+            do
+                for m in "${!size_target[@]}"
+                do
+                    for n in "${!latency_target[@]}"
+                    do
+                        source run_flexnas.sh ${benchmark} ${cd_size[m]} ${size_target[m]} ${latency_target[n]} ${elements[k]} ${loss} ${cd_ops[n]} ${l}
+                    done
+                done
+            done
+fi
+
+if [[ "$1" == "vww" ]]; then
+    # size_target=( 2.0e+04 4.0e+04 6.0e+04 )
+    cd_size=( 5.3e-06 2.8e-06 1.9e-06 1.7e-06 )
+    # latency_target=(2.0e+06 6.0e+06 1.0e+07 )
+    cd_ops=( 5.3e-06 2.8e-06 1.9e-06 1.7e-06 )
+    for i in "${!l[@]}"
+    do
+        for j in "${!loss[@]}"
+        do
+            for k in "${!elements[@]}"
+            do
+                for m in "${!size_target[@]}"
+                do
+                    for n in "${!latency_target[@]}"
+                    do
+                        source m100_offload.sbatch ${cd_size[m]} ${size_target[m]} ${latency_target[n]} ${elements[k]} ${loss[j]} ${cd_ops[n]} ${l[i]} ${benchmark}
+                    done
+                done
+            done
+        done
+    done
+fi
+
+if [[ "$1" == "kws" ]]; then
+    # size_target=( 2.0e+04 4.0e+04 6.0e+04 )
+    cd_size=( 1.0e-05 5.4e-06 3.7e-06 )
+    # latency_target=(2.0e+06 6.0e+06 1.0e+07 )
+    cd_ops=( 1.0e-05 5.4e-06 3.7e-06 )
+    for i in "${!l[@]}"
+    do
+        for j in "${!loss[@]}"
+        do
+            for k in "${!elements[@]}"
+            do
+                for m in "${!size_target[@]}"
+                do
+                    for n in "${!latency_target[@]}"
+                    do
+                        source m100_offload.sbatch ${cd_size[m]} ${size_target[m]} ${latency_target[n]} ${elements[k]} ${loss[j]} ${cd_ops[n]} ${l[i]} ${benchmark}
+                    done
+                done
+            done
+        done
+    done
+fi
