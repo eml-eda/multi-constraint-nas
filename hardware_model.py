2431fd6285027d50d294ef58bd45be0c60dcf2af
Alessio Burrello <aburello@sassauna2.ee.ethz.ch>
2023-03-24 18:30:40 +0100

    code for running experiments with tiny_imagenet and finetuning for layer scheduling on L1/L2

--- hardware_model.py
+++ hardware_model.py
@@ -14,6 +14,15 @@ from flexnas.methods.pit.nn.binarizer import PITBinarizer
 from torchinfo import summary
 from flexnas.methods.pit.nn import PITModule
 
+class FloorSTE(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, ch, N):
+        return torch.floor((ch + N - 1) / N)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output, None
+    
 def _floor(ch, N):
     return math.floor((ch + N - 1) / N)
 
@@ -25,60 +34,56 @@ def compute_layer_latency_GAP8(self, input_shape):
     """
     for layer in self.sn_input_layers:
         stats = summary(layer, input_shape, verbose=0, mode='eval')
+        latency = 0
         try:
-            obj = layer[0]
+            for obj in layer:
+                if obj.__class__.__name__ == "ConvBlock_MN":
+                    obj = obj.conv1
+                if obj.__class__.__name__ == "Conv2d":
+                    ch_in = obj.weight.shape[1]
+                    ch_out = obj.weight.shape[0]
+                    kernel_size_x = obj.weight.shape[2]
+                    kernel_size_y = obj.weight.shape[3]
+                    iterations = _floor(int(input_shape[2]/obj.stride[0]), 2) * _floor(int(input_shape[3]/obj.stride[0]), 8)
+                    im2col = kernel_size_x * kernel_size_y * ch_in * 2
+                    matmul = _floor(ch_out, 4) * (5 + _floor(kernel_size_x * kernel_size_y * ch_in, 4) * (6 + 8) + 10)
+                    latency += iterations * (im2col + matmul)
+                    if obj.groups > 1:
+                        # 1 MAC/cycle
+                        latency = 4 * _floor(ch_out, 4) * int(input_shape[2]/obj.stride[0]) * int(input_shape[3]/obj.stride[0]) * kernel_size_x * kernel_size_y
+                elif obj.__class__.__name__ == "Identity":
+                    latency += 0
+                elif obj.__class__.__name__ == "Linear":
+                    ch_in = obj.weight.shape[1]
+                    ch_out = obj.weight.shape[0]
+                    latency += _floor(ch_in, 2) * _floor(ch_out, 4)
         except:
-            obj = layer
-        if obj.__class__.__name__ == "Conv2d":
-            ch_in = layer[0].weight.shape[1]
-            ch_out = layer[0].weight.shape[0]
-            kernel_size_x = layer[0].weight.shape[2]
-            kernel_size_y = layer[0].weight.shape[3]
-            iterations = _floor(input_shape[2], 2) * _floor(input_shape[3], 8)
-            im2col = kernel_size_x * kernel_size_y * ch_in * 2
-            matmul = _floor(ch_out, 4) * (5 + _floor(kernel_size_x * kernel_size_y * ch_in, 4) * (6 + 8) + 10)
-            latency = iterations * (im2col + matmul)
-            if layer[0].groups > 1:
-                # 1 MAC/cycle
-                latency = 4 * _floor(ch_out, 4) * input_shape[2] * input_shape[3] * kernel_size_x * kernel_size_y
-        elif obj.__class__.__name__ == "Identity":
-            latency = 0
-        elif obj.__class__.__name__ == "Linear":
-            ch_in = layer[0].weight.shape[1]
-            ch_out = layer[0].weight.shape[0]
-            latency = _floor(ch_in, 2) * _floor(ch_out, 4)
-        else:
-            import pdb;pdb.set_trace()
+            if layer.__class__.__name__ == "Identity":
+                obj = layer
+            else:
+                obj = layer.conv1
+            if obj.__class__.__name__ == "Conv2d":
+                ch_in = obj.weight.shape[1]
+                ch_out = obj.weight.shape[0]
+                kernel_size_x = obj.weight.shape[2]
+                kernel_size_y = obj.weight.shape[3]
+                iterations = _floor(int(input_shape[2]/obj.stride[0]), 2) * _floor(int(input_shape[3]/obj.stride[0]), 8)
+                im2col = kernel_size_x * kernel_size_y * ch_in * 2
+                matmul = _floor(ch_out, 4) * (5 + _floor(kernel_size_x * kernel_size_y * ch_in, 4) * (6 + 8) + 10)
+                latency += iterations * (im2col + matmul)
+                if obj.groups > 1:
+                    # 1 MAC/cycle
+                    latency = 4 * _floor(ch_out, 4) * int(input_shape[2]/obj.stride[0]) * int(input_shape[3]/obj.stride[0]) * kernel_size_x * kernel_size_y
+            elif obj.__class__.__name__ == "Identity":
+                latency += 0
+            elif obj.__class__.__name__ == "Linear":
+                ch_in = obj.weight.shape[1]
+                ch_out = obj.weight.shape[0]
+                latency += _floor(ch_in, 2) * _floor(ch_out, 4)
+        import pdb;pdb.set_trace()
         self.layers_macs.append(latency)
 
-def compute_layer_latency_Diana(self, input_shape):
-    """Computes the MACs of each possible layer of the PITSuperNetModule
-    and stores the values in a list.
-    It removes the MACs of the PIT modules contained in each layer because
-    these MACs will be computed and re-added at training time.
-    """
-    for layer in self.sn_input_layers:
-        stats = summary(layer, input_shape, verbose=0, mode='eval')
-        try:
-            obj = layer[0]
-        except:
-            obj = layer
-        if obj.__class__.__name__ == "Conv2d":
-            ch_in = layer[0].weight.shape[1]
-            ch_out = layer[0].weight.shape[0]
-            kernel_size_x = layer[0].weight.shape[2]
-            kernel_size_y = layer[0].weight.shape[3]
-            groups = 1
-            cycles = _floor(ch_out / groups, 16) * ch_in * _floor(input_shape[2], 16) * input_shape[3] * kernel_size_x * kernel_size_y
-            # Works with both depthwise and normal conv:
-            cycles_load_store = input_shape[2] * input_shape[3] * (ch_out + ch_in) / 8
-            latency = (cycles + cycles_load_store)
-        elif obj.__class__.__name__ == "Identity":
-            latency = 0
-        else:
-            import pdb;pdb.set_trace()
-        self.layers_macs.append(latency)
-
+        
 def get_size_binarized_supernet(self) -> torch.Tensor:
     """Method that returns the number of weights for the module
     computed as a weighted sum of the number of weights of each layer.
@@ -119,11 +124,11 @@ def get_latency_conv2D_GAP8(self) -> torch.Tensor:
     cout_mask = self.out_features_masker.theta
     ch_out = torch.sum(PITBinarizer.apply(cout_mask, self._binarization_threshold))
     im2col = self.kernel_size[0] * self.kernel_size[1] * ch_in * 2
-    matmul = _floor(ch_out, 4) * (5 + _floor(self.kernel_size[0] * self.kernel_size[1] * ch_in, 4) * (6 + 8) + 10)
+    matmul = FloorSTE.apply(ch_out, 4) * (5 + FloorSTE.apply(self.kernel_size[0] * self.kernel_size[1] * ch_in, 4) * (6 + 8) + 10)
     latency = iterations * (im2col + matmul)
     if self.groups > 1:
         # 1 MAC/cycle
-        latency = 4 * _floor(ch_out, 4) * self.out_height * self.out_width * self.kernel_size[0] * self.kernel_size[1]
+        latency = 4 * FloorSTE.apply(ch_out, 4) * self.out_height * self.out_width * self.kernel_size[0] * self.kernel_size[1]
     return latency
 
 def get_latency_Linear_GAP8(self) -> torch.Tensor:
@@ -133,31 +138,7 @@ def get_latency_Linear_GAP8(self) -> torch.Tensor:
     cout_mask = self.out_features_masker.theta
     ch_out = torch.sum(PITBinarizer.apply(cout_mask, self._binarization_threshold))
     # Finally compute cost
-    latency = _floor(ch_in, 2) * _floor(ch_out, 4)
-    return latency
-
-def get_latency_conv2D_Diana(self) -> torch.Tensor:
-    cin_mask = self.input_features_calculator.features_mask
-    ch_in = torch.sum(PITBinarizer.apply(cin_mask, self._binarization_threshold))
-    cout_mask = self.out_features_masker.theta
-    ch_out = torch.sum(PITBinarizer.apply(cout_mask, self._binarization_threshold))
-    cycles = _floor(ch_out / self.groups, 16) * ch_in * _floor(self.out_height, 16) * self.out_width * self.kernel_size[0] * self.kernel_size[1]
-    # Works with both depthwise and normal conv:
-    cycles_load_store = self.out_height * self.out_width * (ch_out + ch_in) / 8
-    return cycles_load_store + cycles
-
-
-def get_latency_Linear_Diana(self) -> torch.Tensor:
-    cin_mask = self.input_features_calculator.features_mask
-    ch_in = torch.sum(PITBinarizer.apply(cin_mask, self._binarization_threshold))
-    # Compute actual integer number of output channels
-    cout_mask = self.out_features_masker.theta
-    ch_out = torch.sum(PITBinarizer.apply(cout_mask, self._binarization_threshold))
-    # Finally compute cost
-    cycles = _floor(ch_out, 16) * ch_in
-    # Works with both depthwise and normal conv:
-    cycles_load_store = (ch_out + ch_in) / 8
-    latency = ch_in * ch_out
+    latency = FloorSTE.apply(ch_in, 2) * FloorSTE.apply(ch_out, 4)
     return latency
 
 def get_latency_total(self) -> torch.Tensor:
@@ -167,3 +148,86 @@ def get_latency_total(self) -> torch.Tensor:
         # size += layer.get_size()
         latency = latency + layer.get_latency()
     return latency
+
+def get_latency_conv2D_Diana(self):
+    pass
+
+def get_latency_Linear_Diana(self):
+    pass
+
+def compute_layer_latency_Diana(self):
+    pass
+
+def get_memory_layer_constraints(self) -> torch.Tensor:
+    """Computes the total number of parameters of all NAS-able layers
+    using binary masks
+
+    :return: the total number of parameters
+    :rtype: torch.Tensor
+    """
+    size = torch.tensor(0, dtype=torch.float32)
+    # size = torch.tensor(0)
+    for layer in self._target_layers:
+        # size += layer.get_size()
+        size = size + layer.get_Lx_level_constraint()
+    return size
+
+def get_tot_mem_conv(self) -> torch.Tensor:
+    # Compute actual integer number of input channels
+    cin_mask = self.input_features_calculator.features_mask
+    cin = torch.sum(PITBinarizer.apply(cin_mask, self._binarization_threshold))
+    # Compute actual integer number of output channels
+    cout_mask = self.out_features_masker.theta
+    cout = torch.sum(PITBinarizer.apply(cout_mask, self._binarization_threshold))
+    # Finally compute cost
+    mem_in = (self.out_height * self.stride[0]) * (self.out_width * self.stride[1]) * cin
+    mem_out = self.out_height * self.out_width * cout
+    weights = cin * cout * self.kernel_size[0] * self.kernel_size[1]
+    if self.groups == 1:
+        weights = cin * cout * self.kernel_size[0] * self.kernel_size[1]
+    else:
+        weights = cout * self.kernel_size[0] * self.kernel_size[1]
+    return (mem_in + mem_out + weights ) #- self.TARGET
+
+def get_tot_mem_linear(self) -> torch.Tensor:
+    # Compute actual integer number of input channels
+    cin_mask = self.input_features_calculator.features_mask
+    cin = torch.sum(PITBinarizer.apply(cin_mask, self._binarization_threshold))
+    # Compute actual integer number of output channels
+    cout_mask = self.out_features_masker.theta
+    cout = torch.sum(PITBinarizer.apply(cout_mask, self._binarization_threshold))
+    # Finally compute cost
+    mem_in = cin
+    mem_out = cout
+    weights = cin * cout
+    return (mem_in + mem_out + weights ) #- self.TARGET
+
+def get_Lx_level_constraint_conv(self) -> torch.Tensor:
+    # Compute actual integer number of input channels
+    cin_mask = self.input_features_calculator.features_mask
+    cin = torch.sum(PITBinarizer.apply(cin_mask, self._binarization_threshold))
+    # Compute actual integer number of output channels
+    cout_mask = self.out_features_masker.theta
+    cout = torch.sum(PITBinarizer.apply(cout_mask, self._binarization_threshold))
+    # Finally compute cost
+    mem_in = (self.out_height * self.stride[0]) * (self.out_width * self.stride[1]) * cin
+    mem_out = self.out_height * self.out_width * cout
+    weights = cin * cout * self.kernel_size[0] * self.kernel_size[1]
+    if self.groups == 1:
+        weights = cin * cout * self.kernel_size[0] * self.kernel_size[1]
+    else:
+        weights = cout * self.kernel_size[0] * self.kernel_size[1]
+    return (mem_in + mem_out + weights ) - self.TARGET
+
+def get_Lx_level_constraint_linear(self) -> torch.Tensor:
+    # Compute actual integer number of input channels
+    cin_mask = self.input_features_calculator.features_mask
+    cin = torch.sum(PITBinarizer.apply(cin_mask, self._binarization_threshold))
+    # Compute actual integer number of output channels
+    cout_mask = self.out_features_masker.theta
+    cout = torch.sum(PITBinarizer.apply(cout_mask, self._binarization_threshold))
+    # Finally compute cost
+    mem_in = cin
+    mem_out = cout
+    weights = cin * cout
+    return (mem_in + mem_out + weights ) - self.TARGET
--- utils.py
+++ utils.py
@@ -98,7 +98,6 @@ def train_one_epoch(
                 if args.loss_type == "abs" and "lat_constraint" in args.loss_elements:
                     loss_ops = cd_ops * torch.abs((model.get_latency() - args.latency_target))
                 elif args.loss_type == "max" and "lat_constraint" in args.loss_elements:
-                    import pdb;pdb.set_trace()
                     loss_ops = cd_ops * torch.max((model.get_latency() - args.latency_target), torch.FloatTensor([0]).to(device))[0]
                 elif "lat_obj" in args.loss_elements:
                     loss_ops = cd_ops * model.get_latency()
@@ -141,6 +140,73 @@ def train_one_epoch(
             'loss_task': avglosstask.get(),
             'loss_reg': avglossreg.get(),
             'loss_ops': avglossops.get(),
+            'acc': avgacc.get(),
+        }
+        final_metrics.update(val_metrics)
+        tepoch.set_postfix(final_metrics)
+        tepoch.close()
+        print(final_metrics)
+        print(test_metrics)
+        return final_metrics
+
+# Definition of the function to train for one epoch
+def train_one_epoch_finetuning(
+        epoch: int,
+        search: bool,
+        model: nn.Module,
+        criterion: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        train_dl: DataLoader,
+        val_dl: DataLoader,
+        test_dl: DataLoader,
+        device: torch.device,
+        args,
+        increment_cd_size = None) -> Dict[str, float]:
+    model.train()
+    avgacc = AverageMeter('6.2f')
+    avgloss = AverageMeter('2.5f')
+    avglosstask = AverageMeter('2.5f')
+    avglossreg = AverageMeter('2.5f')
+    step = 0
+    cd_size = min(args.cd_size/100 + increment_cd_size*epoch, args.cd_size)
+    # the goal is to arrive to the final cd_size and cd_ops in 1/5 of the total epochs
+    # starting from 2 orders of magnitude less
+
+    with tqdm(total=len(train_dl), unit="batch") as tepoch:
+        tepoch.set_description(f"Epoch {epoch+1}")
+        for audio, target in train_dl:
+            step += 1
+            tepoch.update(1)
+            audio, target = audio.to(device), target.to(device)
+            output = model(audio)
+            loss_task = criterion(output, target)
+            if search:
+                loss_reg = cd_size * model.get_memory_layer_constraints()
+                loss = loss_task + loss_reg
+            else:
+                loss = loss_task
+                loss_reg = 0
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            acc_val = accuracy(output, target, topk=(1,))
+            avgacc.update(acc_val[0], audio.size(0))
+            avgloss.update(loss, audio.size(0))
+            avglosstask.update(loss_task, audio.size(0))
+            avglossreg.update(loss_reg, audio.size(0))
+            if step % 100 == 99:
+                tepoch.set_postfix({'loss': avgloss,
+                                    'loss_task': avglosstask,
+                                    'loss_reg': avglossreg,
+                                    'acc': avgacc})
+        val_metrics = evaluate(search, model, criterion, val_dl, device)
+        val_metrics = {'val_' + k: v for k, v in val_metrics.items()}
+        test_metrics = evaluate(search, model, criterion, test_dl, device)
+        test_metrics = {'test_' + k: v for k, v in test_metrics.items()}
+        final_metrics = {
+            'loss': avgloss.get(),
+            'loss_task': avglosstask.get(),
+            'loss_reg': avglossreg.get(),
             'acc': avgacc.get(),
         }
         final_metrics.update(val_metrics)
